c754ea01c3d6a0237bccc5a1b75a7958e71e55d1
Maksym Korostelov <flying1519@gmail.com>
2024-05-04 13:46:36 +0300

    stories commited

--- src/components/common/header/Header.stories.tsx
+++ src/components/common/header/Header.stories.tsx
@@ -1,32 +1,32 @@
-import Header from "./Header";
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
-import store from "../../../store/store"
-import "@vars/colors.scss";
-import "@vars/variables.scss";
-import "@mixins/mediaBreakpoints.scss";
-import "../../../styles/global.scss";
-import { Args } from "@storybook/react";
+// import Header from "./Header";
+// import { Provider } from "react-redux";
+// import { BrowserRouter } from "react-router-dom";
+// import store from "../../../store/store"
+// import "@vars/colors.scss";
+// import "@vars/variables.scss";
+// import "@mixins/mediaBreakpoints.scss";
+// import "../../../styles/global.scss";
+// import { Args } from "@storybook/react";
 
 
-export default {
-    title: "Header",
-    component: Header,
-    decorators: [
-          (Story: any) => (
-            <Provider store={store}>
-                <BrowserRouter>
-                    <Story />
-                </BrowserRouter>
-            </Provider>
-        ),
-    ],
-} 
+// export default {
+//     title: "Header",
+//     component: Header,
+//     decorators: [
+//           (Story: any) => (
+//             <Provider store={store}>
+//                 <BrowserRouter>
+//                     <Story />
+//                 </BrowserRouter>
+//             </Provider>
+//         ),
+//     ],
+// } 
 
-const Template: React.FC<Args> = (args) => <Header {...args}/>
+// const Template: React.FC<Args> = (args) => <Header {...args}/>
 
-export const MyHeader =  Template.bind({})
+// export const MyHeader =  Template.bind({})
 
-MyHeader.args = {
+// MyHeader.args = {
     
-}
+// }
--- src/components/ui/buttons/Button.stories.tsx
+++ src/components/ui/buttons/Button.stories.tsx
@@ -1,21 +1,21 @@
-import Button from "./Button";
-import "@vars/colors.scss";
-import "@vars/variables.scss";
-import "@mixins/mediaBreakpoints.scss";
-import "../../../styles/global.scss";
-import { Args } from "@storybook/react";
+// import Button from "./Button";
+// import "@vars/colors.scss";
+// import "@vars/variables.scss";
+// import "@mixins/mediaBreakpoints.scss";
+// import "../../../styles/global.scss";
+// import { Args } from "@storybook/react";
 
 
-export default {
-    title: "Button",
-    component: Button
-}
+// export default {
+//     title: "Button",
+//     component: Button
+// }
 
-const Template: React.FC<Args> = (args) => <Button {...args}/>
+// const Template: React.FC<Args> = (args) => <Button {...args}/>
 
-export const MyButton =  Template.bind({})
+// export const MyButton =  Template.bind({})
 
-MyButton.args = {
-    text: "Order photosession",
-    maxWidth: "320px"
-}
+// MyButton.args = {
+//     text: "Order photosession",
+//     maxWidth: "320px"
+// }
