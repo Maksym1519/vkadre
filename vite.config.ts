ccb3e1dfeb6342aa9da51e30e9556653f187dc99
Maksym Korostelov <flying1519@gmail.com>
2024-05-06 10:08:13 +0300

    m

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vkadre/",
   resolve: {
     alias: {
       components: "/src/components",
